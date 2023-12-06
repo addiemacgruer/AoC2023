8546b748c639acf2ef4e4ccdfd18de256d70f842
Addie MacGruer <addiemacgruer@googlemail.com>
2023-12-06 06:19:06 +0000

    Day 06

---
+++ 06.cc
@@ -0,0 +1,47 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace {
+
+using Size = uint64_t;
+using Parse = std::vector<std::pair<Size, Size>>;
+
+auto test = Parse{{7, 9}, {15, 40}, {30, 200}};
+auto test2 = Parse{{71530, 940200}};
+
+auto puzz = Parse{{47, 282}, {70, 1079}, {75, 1147}, {66, 1062}};
+auto puzz2 = Parse{{47707566, 282107911471062}};
+
+auto find_distance_combos(const Parse &input) {
+  auto rval = Size{1};
+  for (auto &p : input) {
+    auto firstwin = Size{};
+    auto lastwin = Size{};
+    for (auto charge = Size{1}; charge < p.first; ++charge) {
+      auto distance = (p.first - charge) * charge;
+      if (distance > p.second) {
+        if (!firstwin)
+          firstwin = charge;
+        lastwin = charge;
+      } else {
+        if (firstwin)
+          break;
+      }
+    }
+    rval *= (lastwin - firstwin + 1);
+  }
+  return rval;
+}
+
+} // namespace
+
+auto main() -> int {
+  std::cout << "Part 1: " << find_distance_combos(puzz) << '\n';  // 281600
+  std::cout << "Part 2: " << find_distance_combos(puzz2) << '\n'; // 33875953
+}
